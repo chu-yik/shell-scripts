30b80b71a11ad931ef7f9608d4b580bc4686a6ed
MichaelChu <michaelchu@hk01.com>
2018-06-04 09:58:56 +0800

    force simulator to be 6s

--- react-native-ios.sh
+++ react-native-ios.sh
@@ -2,7 +2,7 @@
 #react-native-ios.sh
 
 if [ "$1" = "debug" ]; then
-    react-native run-ios --scheme 'hk01-debug'
+    react-native run-ios --scheme 'hk01-debug' --simulator='iPhone 6s'
 else
-    react-native run-ios --scheme 'hk01-release'
+    react-native run-ios --scheme 'hk01-release' --simulator='iPhone 6s'
 fi
